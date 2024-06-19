e7dba39339bff98a0f082db1edb13bbd3373bf55
milad-a-kareem <milad.kareem@gmail.com>
2024-06-19 03:07:28 +0300

    feat: style header and add asset images

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { LogoComponent } from '../logo/logo.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [LogoComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
