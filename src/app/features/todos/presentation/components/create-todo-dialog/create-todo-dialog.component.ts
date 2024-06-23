f810768196283ff6cca3effcb8c2923a7639b872
milad-a-kareem <milad.kareem@gmail.com>
2024-06-23 16:06:04 +0300

    feat: create add todo & update todo dialog modals

---
+++ src/app/features/todos/presentation/components/create-todo-dialog/create-todo-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-todo-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-todo-dialog.component.html',
+  styleUrl: './create-todo-dialog.component.scss'
+})
+export class CreateTodoDialogComponent {
+
+}
---
+++ src/app/features/todos/presentation/components/update-todo-dialog/update-todo-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-todo-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './update-todo-dialog.component.html',
+  styleUrl: './update-todo-dialog.component.scss'
+})
+export class UpdateTodoDialogComponent {
+
+}
